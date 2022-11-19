2b1efd0c6e6751f13feff940deb6de6a3d68d014
Kenlly-Chacon <kevinleninchacon@hotmail.com>
2022-11-18 19:12:11 -0500

    taller 4

--- src/main/java/com/example/demo/ProyectoU1DbApplication.java
+++ src/main/java/com/example/demo/ProyectoU1DbApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoU1DbApplication {
 
+	//taler 4
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1DbApplication.class, args);
 	}
